daadef2a149fc217ad5e5d674063a8c6816ab25e
UltiimateDog <alan.perezhz@icloud.com>
2025-04-02 11:38:23 +0100

    Ready to map

---
+++ WayZT 3/Views/NewMapView.swift
@@ -0,0 +1,18 @@
+//
+//  NewMapView.swift
+//  WayZT 3
+//
+//  Created by Ultiimate Dog on 02/04/25.
+//
+
+import SwiftUI
+
+struct NewMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewMapView()
+}
